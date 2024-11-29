8c7a21f9baa023e43eda7300e42f90e704b84c45
S4lvatoore <david.sladkov@internet.ru>
2024-11-29 20:20:42 +0400

    Upload to local storage and adding tasks added

--- js/taskmanager.js
+++ js/taskmanager.js
@@ -7,3 +7,20 @@ class Task {
         this.isCompleted = false;
     }
 }
+
+class TasksManager {
+    constructor() {
+        this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    }
+
+    saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+
+    addTask(name, description) {
+        const task = new Task(name, description);
+        this.tasks.push(task);
+        this.saveTasks();
+    }
+
+}
