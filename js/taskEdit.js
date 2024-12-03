59b184e98d6097498c059c875fdc00a9810c840e
S4lvatoore <david.sladkov@internet.ru>
2024-12-03 22:19:11 +0400

    added validation for editing tasks

--- js/taskEdit.js
+++ js/taskEdit.js
@@ -15,10 +15,29 @@ if (!task) {
         const name = document.getElementById('edit-task-name').value.trim();
         const description = document.getElementById('edit-task-description').value.trim();
 
-        if (name && description) {
-            tasksManager.editTask(task.id, name, description);
-            window.location.href = 'index.html';
+        const nameRegex = /^(?!^\d+$)(?!^\s)([A-Za-zА-Яа-я0-9]{1,16})(?:\s([A-Za-zА-Яа-я0-9]{1,16})){1,}$/;
+        const descriptionRegex = /^(?!^\s*$)(?!^\s*.*\b(?:name|task|title)\b.*$).+$/;
+
+        const cleanName = name.replace(/\s+/g, '').toLowerCase();
+        const cleanDescription = description.replace(/\s+/g, '').toLowerCase();
+
+        if (cleanName === cleanDescription) {
+            alert("Error! Description cannot match task name.");
+            return;
         }
+
+        if (!nameRegex.test(name)) {
+            alert("Error! The name must contain at least two words, cannot consist only of numbers, and cannot contain spaces at the beginning or end.");
+            return;
+        }
+
+        if (!descriptionRegex.test(description)) {
+            alert("Error! Description must not match task name.");
+            return;
+        }
+
+        tasksManager.editTask(task.id, name, description);
+        window.location.href = 'index.html';
     });
 
     document.getElementById('cancel-edit').addEventListener('click', () => {
